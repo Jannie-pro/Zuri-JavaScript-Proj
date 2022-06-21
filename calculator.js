7aae71790202f8395a5bf2fcd702f2449efa3212
Janet Ibe <ifyjanette@gmail.com>
2022-06-21 16:30:09 +0100

    Built a basic calculator using JS

---
+++ calculator.js
@@ -0,0 +1,14 @@
+const number1 = 10;
+const number2 = 20;
+
+const add = number1 + number2;
+console.log("Addition of the two numbers is: ", add);
+
+const sub = number1 - number2;
+console.log("Subtraction of the two numbers is: ", sub);
+
+const mul = number1 * number2;
+console.log("Multiplicaton of the two numbers is: ", mul);
+
+const div = number1 / number2;
+console.log("Division of the two numbers is: ", div);
